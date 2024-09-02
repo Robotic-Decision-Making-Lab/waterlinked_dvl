1f14078aca11459a25dc04c992d070c1011e019c
Evan Palmer <evanp922@gmail.com>
2024-09-02 01:44:32 -0700

    why tf does colcon not support fetchcontent

--- waterlinked_driver/src/waterlinked_driver.cpp
+++ waterlinked_driver/src/waterlinked_driver.cpp
@@ -17,3 +17,9 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+#include "waterlinked_driver/waterlinked_driver.hpp"
+
+namespace waterlinked
+{
+}  // namespace waterlinked
