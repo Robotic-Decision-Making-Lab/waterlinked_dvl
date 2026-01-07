0914b9ed9b00ebbe15e889155b1bad8ebe5534a0
cooperrunyan <c@cooperrunyan.com>
2026-01-07 10:50:05 -0800

    Move cmath include up to pass github formatting check

--- waterlinked_dvl_driver/src/waterlinked_dvl_driver.cpp
+++ waterlinked_dvl_driver/src/waterlinked_dvl_driver.cpp
@@ -20,10 +20,10 @@
 
 #include "waterlinked_dvl_driver/waterlinked_dvl_driver.hpp"
 
+#include <cmath>
+
 #include "rclcpp/rclcpp.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-
-#include <cmath>
 
 namespace waterlinked::ros
 {
